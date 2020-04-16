21e3576f460922b583f8c8f127f44fbd838aed08
Rafael Oliveira <rafael.oliveira@spot-edu.com.br>
2020-04-16 19:10:32 -0300

    Create header and footer pages
     - Create header php file
     - Create footer php file
     - Call header and footer file in index php file

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso WordPress</title>
+  </head>
+  <body <?php body_class() ?>>
+    <p>Este é um parágrafo de referência</p>
+    <header>
+      <section class="top-bar">
+        <div class="social-media-icons"></div>
+        <div class="search"></div>
+      </section>
+      <section class="menu-area">
+        <section class="logo"></section>
+        <nav class="menu"></nav>
+      </section>
+    </header>
--- index.php
+++ index.php
@@ -1,16 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Curso WordPress</title>
-  </head>
-  <body <?php body_class() ?>>
-    <p>Este é um parágrafo de referência</p>
-    <header>
-      <section class="top-bar"></section>
-        <div class="social-media-icons"></div>
-        <div class="search"></div>
-    </header>
-  </body>
-</html>
+<?php get_header(); ?>
+    <div class="content-area">
+      <main>
+        <section class="slide"></section>
+        <section class="services"></section>
+        <section class="middle-area">
+          <aside class="sidebar"></aside>
+          <div class="news"></div>
+        </section>
+        <section class="map"></section>
+      </main>
+    </div>
+<?php get_footer(); ?>    
