264c62adfbeade2d1a950f864de7fb9643553d6d
Rafael Oliveira <rafael.oliveira@spot-edu.com.br>
2020-04-16 18:34:28 -0300

    Create theme files
     - Create index php page
     - Create style.css file
     - Add meta data of theme in style.css
     - Create reference code in index.php file

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso WordPress</title>
+  </head>
+  <body <?php body_class() ?>>
+    <p>Este é um parágrafo de referência</p>
+    <header>
+      <section class="top-bar"></section>
+        <div class="social-media-icons"></div>
+        <div class="search"></div>
+    </header>
+  </body>
+</html>
