de8349a5ca727825aa194aeed12668684859029a
Rafael Oliveira <rafael.oliveira@spot-edu.com.br>
2020-04-17 18:24:04 -0300

    Create general template file to contato and sobre files

---
+++ page-home.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+    <div class="content-area">
+      <main>
+        <section class="slide">
+          <div class="container">
+            <div class="row">
+              Slide
+            </div>
+          </div>
+        </section>
+        <section class="services">
+          <div class="container">
+            <div class="row">Serviços</div>
+          </div>
+        </section>
+        <section class="middle-area">
+          <div class="container">
+            <div class="row">
+              <aside class="sidebar col-md-4">Barra Lateral</aside>
+              <div class="news col-md-8">
+                <p>Conteúdo vindo do arquivo home</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="map">
+          <div class="container">
+            <div class="row">Mapa</div>
+          </div>
+        </section>
+      </main>
+    </div>
+<?php get_footer(); ?>
