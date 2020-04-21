5692530970f367f5f9e692622bed3ee5fdcea467
Rafael Oliveira <rafael.oliveira@spot-edu.com.br>
2020-04-21 00:41:01 -0300

    Create first template part
     - Add article tag in first tamplate part

--- index.php
+++ index.php
@@ -25,27 +25,14 @@
               <div class="news col-md-8">
                 <?php 
                   if( have_posts() ):
-                    while( have_posts() ): the_post();
-                ?>
-                  <article>
-                    <h2>
-                      <?php the_title();?>
-                    </h2>
-                    <?php the_post_thumbnail(array( 275, 275 )); ?>
-                    <p>
-                      Published in <?php echo get_the_date(); ?> 
-                      by <?php the_author_posts_link(); ?> 
-                    </p>
-                    <p>Categories: <?php the_category( '' ); ?></p>
-                    <p><?php the_tags( 'Tags: ', ', ' )?></p>
-                    <?php the_content(); ?>
-                  </article>
-                <?php 
-                  endwhile;
+                    while( have_posts() ): 
+                      the_post();
+                      get_template_part( 'template-parts/content' );
+                    endwhile;
                   else:
+                    echo 'There\'s nothing yet to be displayed...';
+                  endif;
                 ?>
-                  <p>There's nothing yet to be displayed...</p>
-                <?php endif; ?>
               </div>
             </div>
           </div>
