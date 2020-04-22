7437171eea6e01116ddc7e5833be5073ffb205e2
Rafael Oliveira <rafael.oliveira@spot-edu.com.br>
2020-04-21 22:36:15 -0300

    Create template part to each type post

--- template-parts/content-image.php
+++ template-parts/content-image.php
@@ -5,7 +5,6 @@
   </h2>
   <?php the_post_thumbnail(array( 275, 275 )); ?>
   <p>
-    Published in <?php echo get_the_date(); ?> 
     by <?php the_author_posts_link(); ?> 
   </p>
   <p>Categories: <?php the_category( '' ); ?></p>
--- template-parts/content-video.php
+++ template-parts/content-video.php
@@ -5,8 +5,7 @@
   </h2>
   <?php the_post_thumbnail(array( 275, 275 )); ?>
   <p>
-    Published in <?php echo get_the_date(); ?> 
-    by <?php the_author_posts_link(); ?> 
+    Published in <?php echo get_the_date(); ?>
   </p>
   <p>Categories: <?php the_category( '' ); ?></p>
   <p><?php the_tags( 'Tags: ', ', ' )?></p>
