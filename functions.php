4a013aef269b0f064be9e0afbcfd9a68fc808fa8
Rafael Oliveira <rafael.oliveira@spot-edu.com.br>
2020-04-21 22:15:19 -0300

    Add post formats
     - Add video and imgage post format

--- functions.php
+++ functions.php
@@ -31,6 +31,8 @@ function wpcurso_config(){
 
   add_theme_support('custom-header', $args);
   add_theme_support('post-thumbnails');
+  add_theme_support('post-formats', array( 'video', 'image' ));
+  
 }
 
 add_action('after_setup_theme', 'wpcurso_config', 0);
