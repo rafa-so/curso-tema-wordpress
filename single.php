eed91232aad76c8040214297a7581cbab67b7af3
Rafael Oliveira <rafael.oliveira@spot-edu.com.br>
2020-04-30 23:18:21 -0300

    add comments session

--- single.php
+++ single.php
@@ -7,6 +7,10 @@
                 while( have_posts() ): 
                     the_post();
                     get_template_part( 'template-parts/content', 'single' );
+
+                    if( comments_open() || get_comments_number() ):
+                        comments_template();
+                    endif;
                 endwhile;
             ?>
         </div>
