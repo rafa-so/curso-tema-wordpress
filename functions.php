bb71f787e70bb702f3b419832f3012b9145301d5
Rafael Oliveira <rafael.oliveira@spot-edu.com.br>
2020-05-08 21:24:39 -0300

    Add social media plugin
     - Add plugin
     - Create widget in sidebar
     - Register sidebar within functions.php file
     - Invoke registered sidebar in header.php file

--- functions.php
+++ functions.php
@@ -101,6 +101,18 @@ function wpcurso_sidebars(){
       'after_title' => '</h2>'
     )
   );
+
+  register_sidebar(
+    array(
+      'name' => 'Social Icons',
+      'id' => 'social-media',
+      'description' => 'Place yout media icons here',
+      'before_widget' => '<div class="widget-wrapper">',
+      'after_widget' => '</div>',
+      'before_title' => '<h2 class="widget-title">',
+      'after_title' => '</h2>'
+    )
+  );
 }
 
 
